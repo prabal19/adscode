66601b99daf136f7b8baf15105fc658664370c3f
prabal19 <prabaltrehan070@gmail.com>
2025-07-08 11:34:21 +0530

    loader file added

---
+++ public/loader.js
@@ -0,0 +1,17 @@
+(function () {
+  const containers = {
+    'full-ad': 'full',
+    'sidebar-ad': 'sidebar',
+    'below-ad': 'below',
+  };
+
+  Object.entries(containers).forEach(([id, type]) => {
+    const el = document.getElementById(id);
+    if (el) {
+      const script = document.createElement('script');
+      script.src = `https://adscode.onrender.com/ads/${type}.js`;
+      script.async = true;
+      el.appendChild(script);
+    }
+  });
+})();
